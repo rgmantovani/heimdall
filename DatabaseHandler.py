136c1fdd2ca36b528e8817b0c1f423b2a5027ae1
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2023-11-01 11:22:42 -0300

    adding abstract class database handler

---
+++ DatabaseHandler.py
@@ -0,0 +1,69 @@
+# -----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
+
+# python abstract class representation 
+from abc import ABC, abstractmethod
+
+from User import *
+from Key import *
+from KeyWithdraw import *
+
+# -----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
+
+class DatabaseHandler(ABC):
+
+    @abstractmethod
+    def searchUserByCode(self, userCode):
+        pass 
+
+    @abstractmethod
+    def searchKeyByCode(self, keyCode):
+        pass
+
+    @abstractmethod
+    def searchWithdrawsByUsercode(self, userCode, status="all"):
+        pass 
+
+    @abstractmethod
+    def searchWithdrawsByKeycode(self, keyCode, status="all"):
+        pass 
+
+    @abstractmethod
+    def searchWithdrawsByStatus(self, status="all"):
+        pass
+
+    @abstractmethod
+    def insertNewUser(self, newUser):
+        pass 
+
+    @abstractmethod
+    def insertNewKey(self, newKey):
+        pass 
+
+    @abstractmethod
+    def withdrawKey(self, keyCode, userCode):
+        pass 
+
+    @abstractmethod
+    def returnKey(self, userCode, keyCode=None):
+        pass 
+
+    @abstractmethod
+    def listAllKeys(self, status):
+        pass 
+
+    @abstractmethod
+    def listAllUsers(self):
+        pass 
+
+    @abstractmethod
+    def listAllKeys(self):
+        pass 
+
+    @abstractmethod
+    def listAllWithdraws(self, status="all"):
+        pass
+
+# -----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
